1c5b23113460b1e52731e4612840dc5680bec329
Hassan <assantebelieve@gmail.com>
2023-12-14 14:26:47 +0200

    fgh

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var navLinks =  document.getElementById("navLinks");
+     function showMenu(){
+      navLinks.style.right = "0";
+    }
+  function hideMenu(){
+       navLinks.style.right = "-200px";
+     }
+
+var typed = new Typed(' .auto-typing',{
+  strings: [' Savoir',
+'Créativité',
+'Développement'],
+typeSpeed: 200,
+backSpeed: 200,
+loop: true,
+fadeOut: true,
+fadeOutClass: 'typed-fade-out',
+fadeOutDelay: 800,})
+
+  
+
+
